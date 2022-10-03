ca5c7ca8ce58cdc22798fabfcd9d4b542ffec7d5
Lucas Donay <lucas.donay@gmail.com>
2022-10-03 14:47:17 -0300

    estrutura criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { HomeComponent } from './compoments/pages/home/home.component';
+import { AboutComponent } from './compoments/pages/about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compoments/header/header.component';
+import { FooterComponent } from './compoments/footer/footer.component';
+import { AboutComponent } from './compoments/pages/about/about.component';
+import { HomeComponent } from './compoments/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
