6d11a3fb4e246ce58c29300d79a31b4c447cfa22
Lucas Donay <lucas.donay@gmail.com>
2022-10-04 15:02:36 -0300

    interfaces

---
+++ src/app/Moments.ts
@@ -0,0 +1,9 @@
+export interface Moment {
+  idade?: number,
+  title: string,
+  description: string,
+  image: string,
+  created_at?: string,
+  updated_At?: string,
+  comments?: [{text: string; username: string}];
+}
