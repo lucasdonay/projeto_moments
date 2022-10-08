278eb3348a94a3e13c7e5d5bcce4c31bcf4ae854
Lucas Donay <lucas.donay@gmail.com>
2022-10-08 14:51:02 -0300

    continuando

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moments';
+import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  allMoments:Moment[] = []
+  moments: Moment[] = []
+  baseApiUrl = environment.baseApiUrl
+
+  //to-do search
+
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
+
+  this.momentService.getMoments().subscribe((items) => {
+    const data = items.data
+
+    data.map((item) => {
+      item.created_at = new Date(item.created_at!).toLocaleDateString(
+        'pt-BR'
+      )
+    })
+
+    this.allMoments = data;
+    this.moments = data;
+
+
+  });
+
   }
 
 }
