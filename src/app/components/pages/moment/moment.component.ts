57b8a6f126347c3c60680aa18eac1de03fc2435c
Lucas Donay <lucas.donay@gmail.com>
2022-10-18 15:38:40 -0300

    finalizando

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -3,6 +3,7 @@ import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/Moments';
 import { Router, ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -14,6 +15,9 @@ export class MomentComponent implements OnInit {
   baseApiUrl = environment.baseApiUrl;
 
   moment?: Moment;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(private momentService: MomentService, private route: ActivatedRoute) { }
 
