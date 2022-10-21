fa2b39536528ed5f328a3f67965eeebb7a1a4915
Lucas Donay <lucas.donay@gmail.com>
2022-10-21 18:00:12 -0300

    edit-moment

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Moment } from 'src/app/Moments';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -15,15 +16,37 @@ export class EditMomentComponent implements OnInit {
 
   constructor(
   private momentService: MomentService,
-  private route: ActivatedRoute
+  private route: ActivatedRoute,
+  private messagesService: MessagesService,
+  private router: Router
   ) { }
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get("id"))
+    const id = Number(this.route.snapshot.paramMap.get('id'))
 
     this.momentService.getMoment(id).subscribe((item) => {
       this.moment = item.data;
-    })
+    });
+  }
+
+  async editHandler(momentData: Moment) {
+
+    const id = this.moment.id
+
+    const formData = new FormData()
+
+    formData.append('title', momentData.title)
+    formData.append('description', momentData.description)
+
+    if(momentData.image) {
+      formData.append('image', momentData.image)
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe()
+
+    this.messagesService.add(`tdasdsadsa`)
+
+    this.router.navigate(['/'])
   }
 
 }
